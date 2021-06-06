bcbdea474c03ad676e2cf1e8e52aca3662f7a94f
tohbay <tobechukwuobitube@gmail.com>
2021-06-06 05:41:25 +0100

    setup Todo App components

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./NewTodoForm.css";
+
+const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="new-todo-form">
+      <input
+        className="new-todo-input"
+        type="text"
+        placeholder="Type you new Todo here"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className="new-todo-button">Create Todo</button>
+    </div>
+  );
+};
+
+export default NewTodoForm;
