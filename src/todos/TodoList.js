54a50f25ad6aed3d375f7d029a2f4179446af8cb
tohbay <tobechukwuobitube@gmail.com>
2021-06-06 11:37:55 +0000

    connect todo app to redux

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { connect } from 'react-redux'
 import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
+import { removeTodo } from "./actions";
 
-const TodoList = ({ todos = [{ text: "Hello" }] }) => (
+const TodoList = ({ todos = [], onRemovePressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
-      <TodoListItem todo={todo} />
+      <TodoListItem todo={todo} onRemovePressed={onRemovePressed}/>
     ))}
   </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+  onRemovePressed: text => dispatch(removeTodo(text))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
